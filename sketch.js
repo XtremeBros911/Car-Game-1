28cac7799f2c70a1f9f535657879ffe567e12024
XtremeBros911 <76610055+XtremeBros911@users.noreply.github.com>
2021-08-28 11:56:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var database, player, form, game;
+var gameState = 0, playerCount;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+
+}
+
+function draw(){
+  
+}
